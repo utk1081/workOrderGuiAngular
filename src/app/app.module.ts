1debb3d0784817021afc167213d97cd9c3aba58b
Moulik Pal <moulik.pal@gloresoft.de>
2022-09-13 17:10:20 +0200

    Adding header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { OverviewComponent } from './overview/overview.component';
 import {HttpClientModule} from '@angular/common/http';
 import { enableProdMode } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OverviewComponent
+    OverviewComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
